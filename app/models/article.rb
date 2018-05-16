9be30cae4a6e7063513d2444257a4e4b5986e2c5
DESKTOP-QKLA90D\enrik <henrydraymundo@gmail.com>
2018-05-16 17:37:17 -0500

    Se termino la parte de agregar un nuevo articulo

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ApplicationRecord
+  validates :title, presence: true  #, length: { minimun: 3, maximun: 50 }
+  validates :description, presence: true #, length: { minimun: 10, maximun: 300 }
+end
