9f898cf65f60a275618a4a7627fca378608e03b2
DESKTOP-QKLA90D\enrik <henrydraymundo@gmail.com>
2018-05-18 15:19:08 -0500

    Se agrego el usuario al articulo

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
-  validates :title, presence: true #, length: { minimun: 3, maximun: 50 }
-  validates :description, presence: true #, length: { minimun: 10, maximun: 300 }
+  belongs_to :user
+  validates :title, presence: true, length: { minimun: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimun: 10, maximum: 300 }
+  validates :user_id, presence: true
 end
